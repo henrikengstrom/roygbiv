842b3860e736f789cbe3db361aac935c0267cd42
Henkee <henkee@henkees-imac-2.local>
2011-10-08 17:29:32 +0200

    Modified Disc and added DiscScatterer class (according to the new pattern)

--- src/main/scala/roygbiv/shape/Disc.scala
+++ src/main/scala/roygbiv/shape/Disc.scala
@@ -15,10 +15,30 @@
 */
 package roygbiv.shape
 
-import roygbiv.math.{Ray, Tuple3f}
+import roygbiv.math.{MathUtils, Ray, Tuple3f}
 
-class Disc extends Shape {
-  def area: Float = null.asInstanceOf[Float]
-  def intersect(ray: Ray): Option[Intersection] = None
-  def getNormalAtPoint(point: Tuple3f): Tuple3f = null
+trait Disc extends Shape {
+  lazy val radiusSquared = radius * radius
+  lazy val area = (scala.math.Pi * radiusSquared).asInstanceOf[Float]
+  def radius: Float
+  def center: Tuple3f
+  def normal: Tuple3f
+
+  def intersect(ray: Ray): Option[Intersection] = {
+    val incidence = ray.direction.dot(normal)
+    val oc = center - ray.origin
+    val t = oc.dot(normal) / incidence
+
+    if (t > MathUtils.Epsilon) {
+      val hitPoint = ray.getPointAtT(t)
+
+      if (hitPoint.distanceSquared(center) < radiusSquared) {
+        Some(Intersection(t, hitPoint, this))
+      }
+    }
+
+    None
+  }
+
+  def getNormalAtPoint(point: Tuple3f): Tuple3f = normal
 }
